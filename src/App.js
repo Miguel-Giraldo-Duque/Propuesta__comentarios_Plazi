68de4123f1b803aa52d09c07ccb6056953223495
Miguel Angel <miguelgiradu@gmail.com>
2024-08-03 16:48:14 -0300

    Agregando responsive

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <h1 className="title">Editor de texto markdown para Platzi</h1>
       <div className="content">
-        <YoutubePlayer width={"600px"} controls url={"https://www.youtube.com/watch?v=R1EEgBJt4gs"}></YoutubePlayer>
+        <div className='player'>
+        <YoutubePlayer width={"100%"} height={"100%"} className="react-player" controls url={"https://www.youtube.com/watch?v=R1EEgBJt4gs"}></YoutubePlayer>
+        </div>
         <MarkdownEditor></MarkdownEditor>
       </div>
   </div>
