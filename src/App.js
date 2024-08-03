4ab43ceb3163d45fcce181b95b179f30eb4057df
Miguel Angel <miguelgiradu@gmail.com>
2024-08-03 16:04:47 -0300

    video tutorial integrado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import YoutubePlayer from 'react-player';
 import './css/App.css';
 import { MarkdownEditor } from './markdowneditor.js';
 function App() {
   return (
     <div className="App">
-      <h1>Editor de texto markdown para Plazi </h1>
-      <MarkdownEditor></MarkdownEditor>
-    </div>
+      <h1 className="title">Editor de texto markdown para Platzi</h1>
+      <div className="content">
+        <YoutubePlayer width={"600px"} controls url={"https://www.youtube.com/watch?v=R1EEgBJt4gs"}></YoutubePlayer>
+        <MarkdownEditor></MarkdownEditor>
+      </div>
+  </div>
   );
 }
 
